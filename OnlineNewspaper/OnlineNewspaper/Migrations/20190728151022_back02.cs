2dca3aa2b7c00476ad391dca463bf225f014236d
Belal Uddin <mdbelalhero@gmail.com>
2019-09-25 19:32:53 +0600

    dot net core project

---
+++ OnlineNewspaper/OnlineNewspaper/Migrations/20190728151022_back02.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineNewspaper.Migrations
+{
+    public partial class back02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
